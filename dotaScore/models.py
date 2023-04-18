959f61e543a4a860874d94d5758df93627dc464c
vladosamerikos <vladosamerikos@gmail.com>
2023-04-18 23:11:25 +0200

    first commit

---
+++ dotaScore/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# example
+
+# class Driver(models.Model):
+
+#     name = models.TextField()
+
+#     license = models.TextField()
+
+# class Car(models.Model):
+
+#     make = models.TextField()
+
+#     model = models.TextField()
+
+#     year = models.IntegerField()
+
+#     vin = models.TextField()
+
+#     owner = models.ForeignKey("Driver", on_delete=models.SET_NULL, null=True)
